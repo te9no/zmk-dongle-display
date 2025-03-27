70bab5791a5658c527dd2fd414579ccb1c39777a
te9no <tatuys9428@gmail.com>
2025-03-27 18:55:06 +0900

    Rename layer_status widget to layer_stats in dongle_display

--- boards/shields/dongle_display/widgets/layer_stats.c
+++ boards/shields/dongle_display/widgets/layer_stats.c
@@ -51,10 +51,10 @@
      };
  }
 
- ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, layer_status_update_cb,
+ ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_stats, struct layer_status_state, layer_status_update_cb,
                              layer_status_get_state)
 
- ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
+ ZMK_SUBSCRIPTION(widget_layer_stats, zmk_layer_state_changed);
 
  int zmk_widget_layer_status_init(struct zmk_widget_layer_status *widget, lv_obj_t *parent) {
      widget->obj = lv_label_create(parent);
