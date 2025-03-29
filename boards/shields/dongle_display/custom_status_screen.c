5a81ad1cfe70bd7e5bca525a41822dbaa2647fe6
te9no <tatuys9428@gmail.com>
2025-03-29 20:34:19 +0900

    Refactor alignment of layer status widget in custom status screen

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -38,8 +38,8 @@ lv_obj_t *zmk_display_status_screen() {
     lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
     
-    zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    // zmk_widget_output_status_init(&output_status_widget, screen);
+    // lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
     
     zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
     lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_RIGHT, 0, -7);
@@ -54,7 +54,9 @@ lv_obj_t *zmk_display_status_screen() {
 
     zmk_widget_layer_status_init(&layer_status_widget, screen);
     // lv_obj_align(zmk_widget_layer_stats_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 2, -18);
-    lv_obj_align_to(zmk_widget_layer_stats_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
+    // lv_obj_align_to(zmk_widget_layer_stats_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
+    // lv_obj_align_to(zmk_widget_layer_stats_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
+    lv_obj_align(zmk_widget_layer_stats_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
     lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
