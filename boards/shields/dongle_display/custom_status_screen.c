8eb1ed829eeac0411b83938e995d46c7518700fc
te9no <tatuys9428@gmail.com>
2025-04-08 14:54:40 +0900

    refactor: update widget initialization to use a container for better layout management

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -29,38 +29,39 @@ lv_style_t global_style;
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
-
     screen = lv_obj_create(NULL);
-
-    //lv_disp_set_rotation(lv_disp_get_default(), LV_DISP_ROT_90);
 
     lv_style_init(&global_style);
     lv_style_set_text_font(&global_style, &lv_font_unscii_8);
     lv_style_set_text_letter_space(&global_style, 1);
     lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
+
+    // Create a container for widgets
+    lv_obj_t *row = lv_obj_create(screen);
+    lv_obj_set_size(row, 128, 32);
+    lv_obj_set_style_pad_all(row, 0, LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(row, LV_OPA_TRANSP, LV_PART_MAIN);
+    lv_obj_set_flex_flow(row, LV_FLEX_FLOW_ROW);
+    lv_obj_set_style_transform_angle(row, 900, 0);
+    lv_obj_center(row);
     
-    zmk_widget_output_status_init(&output_status_widget, screen);
+    zmk_widget_output_status_init(&output_status_widget, row);
     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
-    lv_obj_set_style_transform_angle(zmk_widget_output_status_obj(&output_status_widget), 900, 0); // 90 degrees * 10
     
-    zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
+    zmk_widget_bongo_cat_init(&bongo_cat_widget, row);
     lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_CENTER, 0, 0);
-    lv_obj_set_style_transform_angle(zmk_widget_bongo_cat_obj(&bongo_cat_widget), 900, 0);
 
 #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-    zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
+    zmk_widget_hid_indicators_init(&hid_indicators_widget, row);
     lv_obj_align(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_BOTTOM_RIGHT, -2, -2);
-    lv_obj_set_style_transform_angle(zmk_widget_hid_indicators_obj(&hid_indicators_widget), 900, 0);
 #endif
 
-    zmk_widget_layer_stats_init(&layer_status_widget, screen);
+    zmk_widget_layer_stats_init(&layer_status_widget, row);
     lv_obj_align(zmk_widget_layer_stats_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 2, -2);
-    lv_obj_set_style_transform_angle(zmk_widget_layer_stats_obj(&layer_status_widget), 900, 0);
 
-    zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
+    zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, row);
     lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
-    lv_obj_set_style_transform_angle(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), 900, 0);
 
     return screen;
 }
