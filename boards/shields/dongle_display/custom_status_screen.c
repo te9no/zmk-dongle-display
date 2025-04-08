a87065aae686d9fd2571fb2a51dfa4e22f8c30e7
te9no <tatuys9428@gmail.com>
2025-04-08 16:46:27 +0900

    revert

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -11,7 +11,6 @@
 #include "widgets/layer_status.h"
 #include "widgets/output_status.h"
 #include "widgets/hid_indicators.h"
-#include <lvgl.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -30,40 +29,39 @@ lv_style_t global_style;
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
+
     screen = lv_obj_create(NULL);
+
+    //lv_disp_set_rotation(lv_disp_get_default(), LV_DISP_ROT_90);
 
     lv_style_init(&global_style);
     lv_style_set_text_font(&global_style, &lv_font_unscii_8);
     lv_style_set_text_letter_space(&global_style, 1);
     lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
-
-    // Create a container for widgets
-    lv_obj_t *container = lv_obj_create(screen);
-    lv_obj_set_size(container, 32, 128);  // サイズを回転後の寸法に合わせる
-    lv_obj_set_style_pad_all(container, 0, LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(container, LV_OPA_TRANSP, LV_PART_MAIN);
-    lv_obj_set_pos(container, 0, 0);  // 位置を左上に固定
-
-    zmk_widget_output_status_init(&output_status_widget, container);
-    lv_obj_set_pos(zmk_widget_output_status_obj(&output_status_widget), 0, 0);
     
-    zmk_widget_bongo_cat_init(&bongo_cat_widget, container);
-    lv_obj_set_pos(zmk_widget_bongo_cat_obj(&bongo_cat_widget), 0, 48);
+    zmk_widget_output_status_init(&output_status_widget, screen);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    
+    zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
+    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_RIGHT, 0, -2);
+
+    // zmk_widget_modifiers_init(&modifiers_widget, screen);
+    // lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
 
 #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
-    zmk_widget_hid_indicators_init(&hid_indicators_widget, container);
-    lv_obj_set_pos(zmk_widget_hid_indicators_obj(&hid_indicators_widget), 0, 96);
+    zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
+    lv_obj_align_to(zmk_widget_hid_indicators_obj(&hid_indicators_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_TOP_LEFT, 0, -2);
 #endif
 
-    zmk_widget_layer_stats_init(&layer_status_widget, container);
-    lv_obj_set_pos(zmk_widget_layer_stats_obj(&layer_status_widget), 0, 106);
+    zmk_widget_layer_stats_init(&layer_status_widget, screen);
+    // lv_obj_align(zmk_widget_layer_stats_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 2, -18);
+    // lv_obj_align_to(zmk_widget_layer_stats_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
+    // lv_obj_align_to(zmk_widget_layer_stats_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
+    lv_obj_align(zmk_widget_layer_stats_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
 
-    zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, container);
-    lv_obj_set_pos(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), 0, 116);
-
-    // スクリーン全体を回転
-    lv_obj_set_style_transform_angle(screen, 900, 0);
+    zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
+    lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
 
     return screen;
 }
